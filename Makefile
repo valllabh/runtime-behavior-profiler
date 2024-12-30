223ea01151d0af955b17a731be1f34ae47dc5713
vallabh <1380940+valllabh@users.noreply.github.com>
2024-12-30 12:07:36 +0000

    resolving error in main

--- Makefile
+++ Makefile
@@ -1,7 +1,4 @@
 # compile binary
 
 build:
-	go build -o bin/tetragon-event-listener pkg/main.go
-
-build-with-version:
-	go build -ldflags "-X main.version=$(VERSION)" -o bin/tetragon-event-listener pkg/main.go
+	go build -o bin/tetragon-event-listener main.go
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"fmt"
 	eventprocessortetragon "runtime-behavior-profiler/pkg/event/processor/tetragon"
 )
 
-var version string
-
 func main() {
-	fmt.Printf("Version: %s\n", version)
 	err := eventprocessortetragon.ListenToEvents()
 	if err != nil {
 		println(err.Error())
